b298fdb6c18e9be21a42f6d382515a1b901bf157
Amit Vasudevan <amitvasudevan@acm.org>
2017-09-22 12:02:20 -0500

    uberspark: tools: common: prepare test harness to experiment with manifest parsing...

--- uberspark/tools/common/test.ml
+++ uberspark/tools/common/test.ml
@@ -4,7 +4,9 @@
 *)
 
 open Uslog
+open Sys
 
+(*
 let main () =
 	begin
 		Uslog.current_level := Uslog.ord Uslog.Info;
@@ -13,6 +15,26 @@ let main () =
 	;;
 		
 main ();;
+*)
+
+
+let main () =
+	Uslog.current_level := Uslog.ord Uslog.Info;
+
+	let len = Array.length Sys.argv in
+		if len = 2 then
+	    	begin
+					Uslog.logf "test" Uslog.Info "Manifest file: %s" Sys.argv.(1);
+				end
+		else
+				begin
+					Uslog.logf "test" Uslog.Info "Insufficient Parameters!";
+				end
+
+	;;
+		
+main ();;
+
 
 
 
