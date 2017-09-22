0381b8d14a8342f584c20e299933991520866bc7
Amit Vasudevan <amitvasudevan@acm.org>
2017-09-22 13:02:07 -0500

    uberspark: tools: common: parse namespace tag in manifest

--- uberspark/tools/common/test.ml
+++ uberspark/tools/common/test.ml
@@ -24,14 +24,15 @@ main ();;
 let parse_json filename = 
 	Uslog.logf "test" Uslog.Info "Manifest file: %s" filename;
 
-	(* Read JSON file into an OCaml string *)
-  let buf = In_channel.read_all filename in
-  (* Use the string JSON constructor *)
-  let json1 = Yojson.Basic.from_string buf in
-  (* Use the file JSON constructor *)
-  let json2 = Yojson.Basic.from_file filename in
-  (* Test that the two values are the same *)
-  print_endline (if json1 = json2 then "OK" else "FAIL")
+  (* read the manifest JSON *)
+  let json = Yojson.Basic.from_file filename in
+
+	  (* Locally open the JSON manipulation functions *)
+	  let open Yojson.Basic.Util in
+	  	let ns = json |> member "ns" |> to_string in
+
+			  (* Print the results of the parsing *)
+			  Uslog.logf "test" Uslog.Info "Namespace (ns): %s\n" ns;
 	
 	;;
 
