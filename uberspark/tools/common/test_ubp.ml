08a05f6b584979618aca813a3b9da81117800423
Amit Vasudevan <amitvasudevan@acm.org>
2018-01-05 10:16:17 -0600

    tools: common: cleanup test_bp.ml

--- uberspark/tools/common/test_ubp.ml
+++ uberspark/tools/common/test_ubp.ml
@@ -163,47 +163,14 @@ let populate_uobj_callmasks uobj_entry uobj_id =
 
 ;;
 
-
-let parse_ubp_entry entry = 
-(*	try
-		 	let open Yojson.Basic.Util in
-			let uobj_0 = entry in 
-*)			
-			(*let slabname = uobj_0 |> member "name" |> to_string in
-  		let slabdir = uobj_0 |> member "dir" |> to_string in
-  		let slabtype = uobj_0 |> member "type" |> to_string in
-  		let slabsubtype = uobj_0 |> member "subtype" |> to_string in
-  		let slabgsmfile = !g_rootdir ^ slabdir ^ "/" ^ slabname ^ ".gsm.pp" in
-	    let slabmmapfile = !g_rootdir ^ "_objects/_objs_slab_" ^ slabname ^ "/" ^ slabname ^ ".mmap" in
-	
+(*
 			let uobj_0_uapifn = uobj_0 |> member "uobj-uapifn" |> to_list in
 			let uobj_0_uapifn_name = myMap uobj_0_uapifn ~f:(fun uobj_0 -> member "name" uobj_0 |> to_string) in 
   		let uobj_0_uapifn_id = myMap uobj_0_uapifn ~f:(fun uobj_0 -> member "id" uobj_0 |> to_string) in 
   		let uobj_0_uapifn_opt1 = myMap uobj_0_uapifn ~f:(fun uobj_0 -> member "opt1" uobj_0 |> to_string) in 
   		let uobj_0_uapifn_opt2 = myMap uobj_0_uapifn ~f:(fun uobj_0 -> member "opt2" uobj_0 |> to_string) in 
+*) 
 
-		  Uslog.logf "test" Uslog.Info "uobj_0: name: %s" slabname;
-			(*List.iter dbg_dump_string uobj_0_uapifn_name;*)
-
-			Hashtbl.add slab_idtodir !g_totalslabs slabdir;
-			Hashtbl.add slab_idtoname !g_totalslabs slabname;
-			Hashtbl.add slab_idtotype !g_totalslabs slabtype;
-			Hashtbl.add slab_idtosubtype !g_totalslabs slabsubtype;
-			Hashtbl.add slab_idtogsm !g_totalslabs slabgsmfile;
-			Hashtbl.add slab_idtommapfile !g_totalslabs slabmmapfile;
-			Hashtbl.add slab_nametoid slabname !g_totalslabs;
-			*)
-
-			populate_uobj_characteristics entry;
-			populate_uobj_callmasks entry !g_totalslabs;						
-			g_totalslabs := !g_totalslabs + 1;
-			
-(*					
-	with Yojson.Json_error s -> 
-			Uslog.logf "test" Uslog.Info "ERROR in parsing manifest!";
-	;
-*)
-;;
 
 
 (*
@@ -212,55 +179,11 @@ let parse_ubp_entry entry =
 	**************************************************************************
 *)
 
-let compute_uapiandcallmasks totalslabs =
-	let i = ref 0 in
-	
-		(* now iterate through all the slab id's and populate callmask and uapimasks *)
-		i := 0;
-		while (!i < !g_totalslabs) do
-	    	begin
-			  Uslog.logf "test" Uslog.Info "  slabdir=%s\n" (Hashtbl.find slab_idtodir !i);      			
-				Uslog.logf "test" Uslog.Info "  slabname=%s\n" (Hashtbl.find slab_idtoname !i);      			
-			  Uslog.logf "test" Uslog.Info "  slabtype=%s\n" (Hashtbl.find slab_idtotype !i);      			
-			  Uslog.logf "test" Uslog.Info "  slabsubtype=%s\n" (Hashtbl.find slab_idtosubtype !i);      			
-			  Uslog.logf "test" Uslog.Info "  slabgsmfile=%s\n" (Hashtbl.find slab_idtogsm !i);      			
-			  Uslog.logf "test" Uslog.Info "  slabmmapfile=%s\n" (Hashtbl.find slab_idtommapfile !i);      			
-
-				(*
-					umfcommon_parse_mmap should be a standalone function not here
-					it should just populate mmap structure for all uobjs that are not richguest 
-					type
-				*) 
-			
-				(* if (g_memoffsets && ((compare (Hashtbl.find slab_idtosubtype !i) "XRICHGUEST") <> 0) ) then
-					begin
-						umfcommon_parse_mmap (Hashtbl.find slab_idtommapfile !i) !i !g_totalslabs;
-						Hashtbl.add slab_idtouapifnmask !i (umfcommon_parse_gsm (Hashtbl.find slab_idtogsm !i) !i !g_totalslabs true);
-					end
-				else
-					begin
-				*)
-				
-				(* Hashtbl.add slab_idtouapifnmask !i (umfcommon_parse_gsm (Hashtbl.find slab_idtogsm !i) !i !g_totalslabs false); *)
-				
-				(*
-					end
-				;	
-				*)			    	
-
-
-
-	    	i := !i + 1;
-				end
-		done;
-;;
-
 
 let parse_ubp filename = 
-	
 	try
 
-	(* read the manifest JSON *)
+		(* read the manifest JSON *)
   let json = Yojson.Basic.from_file filename in
 
 	  (* Locally open the JSON manipulation functions *)
@@ -302,8 +225,7 @@ let main () =
 					Uslog.logf "test" Uslog.Info "g_memoffsets=%u\n" !g_memoffsets;
 					Uslog.logf "test" Uslog.Info "g_rootdir=%s\n" !g_rootdir;
 					parse_ubp Sys.argv.(1);
-					Uslog.logf "test" Uslog.Info "g_totalslabs=%u\n" !g_totalslabs;
-					compute_uapiandcallmasks !g_totalslabs;
+					Uslog.logf "test" Uslog.Info "All done!\n";
 				end
 		else
 				begin
