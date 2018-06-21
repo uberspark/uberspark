9839fd67bd4ede7093d4370e6e52f011a4cf04d3
Amit Vasudevan <amitvasudevan@acm.org>
2018-06-21 13:29:43 -0500

    libusmf: flesh out test module

---
+++ uberspark/tools/common/libusmf_test.ml
@@ -0,0 +1,36 @@
+(*
+	test module for libusmf: uobj manifest parsing library
+	author: amit vasudevan (amitvasudevan@acm.org)
+*)
+
+open Uslog
+open Libusmf
+open Sys
+open Str
+
+let main () =
+	Uslog.current_level := Uslog.ord Uslog.Info;
+
+	let len = Array.length Sys.argv in
+		if len = 3 then
+	    	begin
+					let uobj_mf_filename = ref "" in
+					let uobj_id = ref 0 in
+						uobj_mf_filename := Sys.argv.(1);
+						uobj_id := int_of_string(Sys.argv(2));
+						Uslog.logf "libusmf_test" Uslog.Info "uobj manifest file: %s" !uobj_mf_filename;
+						Uslog.logf "libusmf_test" Uslog.Info "uobj id=%u\n" !uobj_id;
+						(* parse manifest *)
+						Uslog.logf "libusmf_test" Uslog.Info "All done!\n";
+				end
+		else
+				begin
+					Uslog.logf "libusmf_test" Uslog.Info "Insufficient Parameters!";
+				end
+
+	;;
+		
+main ();;
+
+
+
