fc739386b3734f6f4fa28ef705ce4649ee0351ab
Amit Vasudevan <amitvasudevan@acm.org>
2018-06-21 17:37:37 -0500

    libusmf: parse uobj base characteristics

--- uberspark/tools/common/libusmf/libusmf.ml
+++ uberspark/tools/common/libusmf/libusmf.ml
@@ -12,6 +12,14 @@ module Libusmf =
 	global variables
 	**************************************************************************
 *)
+let slab_idtoname = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
+let slab_idtotype = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
+let slab_idtosubtype = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
+
+let slab_nametoid = ((Hashtbl.create 32) : ((string,int)  Hashtbl.t));;
+let slab_idtodir = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
+let slab_idtogsm = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
+let slab_idtommapfile = ((Hashtbl.create 32) : ((int,string)  Hashtbl.t));;
 
 
 (*
@@ -66,7 +74,6 @@ let rec myMap ~f l = match l with
 
 
 
-
 (*
 	**************************************************************************
 	main interfaces
@@ -83,8 +90,23 @@ let usmf_parse_uobj_mf uobj_mf_filename uobj_id =
 
 	  (* Locally open the JSON manipulation functions *)
 	  let open Yojson.Basic.Util in
-			Uslog.logf "libusmf" Uslog.Info "name=%s, id=%u" uobj_mf_filename uobj_id;
-												
+		 	let uobj_name = json |> member "uobj-name" |> to_string in
+		 	let uobj_type = json |> member "uobj-type" |> to_string in
+		 	let uobj_subtype = json |> member "uobj-subtype" |> to_string in
+			let uobj_dir = (Filename.dirname uobj_mf_filename) in
+    	let uobj_mmapfile = uobj_dir ^ "_objects/_objs_slab_" ^ uobj_name ^ "/" ^ uobj_name ^ ".mmap" in
+
+				Uslog.logf "libusmf" Uslog.Info "uobj-name:%s" uobj_name;
+				Uslog.logf "libusmf" Uslog.Info "uobj-type:%s" uobj_type;
+				Uslog.logf "libusmf" Uslog.Info "uobj-subtype:%s" uobj_subtype;
+				Hashtbl.add slab_idtoname uobj_id uobj_name;
+				Hashtbl.add slab_idtotype uobj_id uobj_type;
+				Hashtbl.add slab_idtosubtype uobj_id uobj_subtype;
+				Hashtbl.add slab_nametoid uobj_name uobj_id;
+				Hashtbl.add slab_idtodir uobj_id uobj_dir;
+				Hashtbl.add slab_idtogsm uobj_id uobj_mf_filename;
+				Hashtbl.add slab_idtommapfile uobj_id uobj_mmapfile;
+	
 	with Yojson.Json_error s -> 
 		Uslog.logf "test" Uslog.Info "ERROR in parsing manifest!";
 	;
