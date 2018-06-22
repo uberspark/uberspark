8074ac2bdfb262e3a41ee67d4354cf6bc4b868f0
Amit Vasudevan <amitvasudevan@acm.org>
2018-06-22 10:51:11 -0500

    libusmf: update test rig to parse uobj list before parsing uobj manifest; successful test.

--- uberspark/tools/common/libusmf_test.ml
+++ uberspark/tools/common/libusmf_test.ml
@@ -12,15 +12,18 @@ let main () =
 	Uslog.current_level := Uslog.ord Uslog.Info;
 
 	let len = Array.length Sys.argv in
-		if len = 3 then
+		if len = 4 then
 	    	begin
+					let uobj_list_filename = ref "" in
 					let uobj_mf_filename = ref "" in
 					let uobj_id = ref 0 in
-						uobj_mf_filename := Sys.argv.(1);
-						uobj_id := int_of_string(Sys.argv.(2));
+						uobj_list_filename := Sys.argv.(1);
+						uobj_mf_filename := Sys.argv.(2);
+						uobj_id := int_of_string(Sys.argv.(3));
+						Uslog.logf "libusmf_test" Uslog.Info "uobj list file: %s" !uobj_list_filename;
 						Uslog.logf "libusmf_test" Uslog.Info "uobj manifest file: %s" !uobj_mf_filename;
 						Uslog.logf "libusmf_test" Uslog.Info "uobj id=%u\n" !uobj_id;
-						(* parse manifest *)
+						Libusmf.usmf_parse_uobj_list !uobj_list_filename;
 						Libusmf.usmf_parse_uobj_mf !uobj_mf_filename !uobj_id;
 						Uslog.logf "libusmf_test" Uslog.Info "All done!\n";
 				end
