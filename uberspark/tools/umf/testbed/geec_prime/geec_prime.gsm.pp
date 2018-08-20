54150f6ad63f9ce65be4f9adba79704d071ea2fc
Amit Vasudevan <amitvasudevan@acm.org>
2018-08-20 15:04:11 -0500

    umfparse: refine manifest for prime and uapi_slabmempgtbl and update top-level uobjlst file

--- uberspark/tools/umf/testbed/geec_prime/geec_prime.gsm.pp
+++ uberspark/tools/umf/testbed/geec_prime/geec_prime.gsm.pp
@@ -1,29 +1,92 @@
-// slab dependencies
-#include <geec_prime.h>
-#include <geec_sentinel.h>
-#include <uapi_slabmempgtbl.h>
-#include <uapi_sysdata.h>
-#include <xc_init.h>
+{
+	"uobj-name": "geec_prime",
+	"uobj-type": "VfT_SLAB",
+	"uobj-subtype": "PRIME",
 
-//slab memory sections
-MS:CODE:0x200000:void:void
-MS:DATA:0x3800000:void:void
-MS:STACK:0xe00000:void:void
-MS:DMADATA:0x200000:void:void
+	"uobj-uapifunctions":[],
+
+	"uobj-callees": "	geec_sentinel
+						uapi_slabmempgtbl
+						xc_init				
+						uapi_sysdata
+						",
+
+	"uobj-uapicallees":[
+		{ 
+			"uobj-name": "uapi_slabmempgtbl",
+			"uobj-uapifunctionid": "1", 
+			"opt1" : "(void)0;",
+			"opt2" : "(1)" 
+		},
+		{ 
+			"uobj-name": "uapi_slabmempgtbl",
+			"uobj-uapifunctionid": "0", 
+			"opt1" : "(void)0;",
+			"opt2" : "(1)" 
+		},
+		{ 
+			"uobj-name": "uapi_sysdata",
+			"uobj-uapifunctionid": "1", 
+			"opt1" : "(void)0;",
+			"opt2" : "(1)" 
+		}
+	],	
 
 
-S:geec_sentinel:void:void:void
-S:uapi_slabmempgtbl:void:void:void
-S:xc_init:void:void:void
-S:uapi_sysdata:void:void:void
+	"uobj-resource-devices":[
+		{ 
+			"type": "include",
+			"opt1" : "0x0000",
+			"opt2" : "0x0000" 
+		},
+		{ 
+			"type": "include",
+			"opt1" : "0x0000",
+			"opt2" : "0x0000" 
+		},
+		{ 
+			"type": "include",
+			"opt1" : "0x0000",
+			"opt2" : "0x0000" 
+		},
+		{ 
+			"type": "include",
+			"opt1" : "0x0000",
+			"opt2" : "0x0000" 
+		}
+	],	
 
-U:uapi_slabmempgtbl:XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR:(void)0;:(1)
-U:uapi_slabmempgtbl:XMHFGEEC_UAPI_SLABMEMPGTBL_INITMEMPGTBL:(void)0;:(1)
-U:uapi_sysdata:UXMHF_UAPI_SYSDATA_E820ADDENTRY:(void)0;:(1)
+	"uobj-resource-memory":[],	
+
+	"uobj-exportfunctions": "",
 
 
-RD:INCL:PCI_VENDOR_ID_XMHFGEEC:PCI_DEVICE_ID_XMHFGEEC_LAPIC:void
-RD:INCL:PCI_VENDOR_ID_XMHFGEEC:PCI_DEVICE_ID_XMHFGEEC_TXT:void
-RD:INCL:PCI_VENDOR_ID_XMHFGEEC:PCI_DEVICE_ID_XMHFGEEC_TPM:void
-RD:INCL:PCI_VENDOR_ID_XMHFGEEC:PCI_DEVICE_ID_XMHFGEEC_IOMMU:void
+	"uobj-binary-sections":[
+		{ 
+			"section-name": "code",
+			"section-size": "0x200000"
+		},
+		{ 
+			"section-name": "data",
+			"section-size": "0x3800000"
+		},
+		{ 
+			"section-name": "stack",
+			"section-size": "0xe00000"
+		},
+		{ 
+			"section-name": "dmadata",
+			"section-size": "0x200000"
+		}
+	],	
+
+	
+	"c-files":	"",
+		
+	"v-harness": []
+		
+	
+}
+
+
 
