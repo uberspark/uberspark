e1dc9b255daf865e3fa4bc8042bb76f421541d0f
Amit Vasudevan <amitvasudevan@acm.org>
2018-10-08 19:31:09 -0500

    add new module usextbinutils to interface with external binary generation tools

---
+++ src/tools/libs/usextbinutils/usextbinutils.ml
@@ -0,0 +1,13 @@
+(*
+	uberSpark external binary generation utilities interface
+	author: amit vasudevan (amitvasudevan@acm.org)
+*)
+
+module UsExtbinutils =
+	struct
+		
+	let test_func () =
+		(true)
+	;;
+
+	end
