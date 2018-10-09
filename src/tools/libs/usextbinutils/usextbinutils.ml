9090f1909823b5f3c832e91b7fd459de0180f178
Amit Vasudevan <amitvasudevan@acm.org>
2018-10-09 15:07:53 -0500

    usextbinutils: add interface preprocess to preprocess a file with given list of includes and defines

--- src/tools/libs/usextbinutils/usextbinutils.ml
+++ src/tools/libs/usextbinutils/usextbinutils.ml
@@ -3,9 +3,31 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 *)
 
+open UsOsservices
+
 module UsExtbinutils =
 	struct
-		
+
+	let tool_pp = "gcc" ;;
+			
+	let preprocess 
+			pp_inputfilename pp_includedirs_list pp_defines_list =
+		  let pp_outputfilename = 
+				((Filename.basename pp_inputfilename) ^ ".upp") in
+			let pp_cmdline = ref [] in
+				pp_cmdline := !pp_cmdline @ [ "-E" ];
+				pp_cmdline := !pp_cmdline @ [ "-P" ];
+				pp_cmdline := !pp_cmdline @ pp_defines_list;
+				pp_cmdline := !pp_cmdline @ pp_includedirs_list;
+				pp_cmdline := !pp_cmdline @ [ pp_inputfilename ];
+				pp_cmdline := !pp_cmdline @ [ "-o" ];
+				pp_cmdline := !pp_cmdline @ [ pp_outputfilename ];
+				ignore(UsOsservices.exec_process_withlog 
+					tool_pp !pp_cmdline true);
+		(pp_outputfilename)
+	;;
+				
+								
 	let test_func () =
 		(true)
 	;;
