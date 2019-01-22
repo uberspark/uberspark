47f38f9562bea44762b69e85252b0a4d70c3ef70
Amit Vasudevan <amitvasudevan@acm.org>
2019-01-22 12:13:31 -0600

    tools/libs: usuobj: revise build interface to parse manifest and dump source file counts

--- src/tools/libs/usuobj/usuobj.ml
+++ src/tools/libs/usuobj/usuobj.ml
@@ -1,97 +1,78 @@
 (*------------------------------------------------------------------------------
-	uberSpark uberobject library verification and build interface
+	uberSpark uberobject verification and build interface
 	author: amit vasudevan (amitvasudevan@acm.org)
 ------------------------------------------------------------------------------*)
 
 open Uslog
 open Usmanifest
 
-module Usuobjlib =
+module Usuobj =
 	struct
 
-	let log_tag = "Usuobjlib";;
+	let log_tag = "Usuobj";;
 
-	let usmf_type_usuobjlib = "uobjlib";;
+	let usmf_type_usuobj = "uobj";;
 
 
 	(*--------------------------------------------------------------------------*)
-	(* build a uobjlib *)
-	(* uobjlib_manifest_filename = uobjlib manifest filename *)
+	(* build a uobj *)
+	(* uobj_manifest_filename = uobj manifest filename *)
 	(* build_dir = directory to use for building *)
 	(* keep_temp_files = true if temporary files need to be preserved in build_dir *)
 	(*--------------------------------------------------------------------------*)
 	let build 
 				uobjlib_manifest_filename build_dir keep_temp_files = 
+
+		Uslog.logf log_tag Uslog.Info "Starting...\n";
 		
 		let usmf_type = ref "" in
 		let (retval, mf_json) = Usmanifest.read_manifest 
 															uobjlib_manifest_filename keep_temp_files in
 			if (retval == false) then
 				begin
-					Uslog.logf log_tag Uslog.Error "could not read uobjlib manifest.";
+					Uslog.logf log_tag Uslog.Error "could not read uobj manifest.";
 					ignore (exit 1);
 				end
 			;		
 
-			Uslog.logf log_tag Uslog.Info "Done.\n";
+		Uslog.logf log_tag Uslog.Info "Parsed uobj manifest.\n";
 
-			let (rval, usmf_hdr_type, usmf_hdr_subtype, usmf_hdr_id) =
-					Usmanifest.parse_node_usmf_hdr mf_json in
-				
-			if (rval == false) then
-				begin
-					Uslog.logf log_tag Uslog.Error "invalid manifest hdr.";
-					ignore (exit 1);
-				end
-			;
-				
-			if (compare usmf_hdr_type usmf_type_usuobjlib) <> 0 then
-				begin
-					Uslog.logf log_tag Uslog.Error "invalid uobjlib manifest type '%s'." !usmf_type;
-					ignore (exit 1);
-				end
-			;
+		let (rval, usmf_hdr_type, usmf_hdr_subtype, usmf_hdr_id) =
+				Usmanifest.parse_node_usmf_hdr mf_json in
 			
-						
-			let(rval, uobjlib_cfiles, uobjlib_casmfiles) = 
-				Usmanifest.parse_node_usmf_sources	mf_json in
-
-			if (rval == false) then
-				begin
-					Uslog.logf log_tag Uslog.Error "invalid usmf-sources node in manifest.";
-					ignore (exit 1);
-				end
-			;
-
-			Uslog.logf log_tag Uslog.Info "cfiles_count=%u, casmfiles_count=%u\n"
-						(List.length uobjlib_cfiles) (List.length uobjlib_casmfiles);
-
-
-			let(rval, uobjlib_vharness_list) = 
-				Usmanifest.parse_node_usmf_vharness	mf_json in
-
-			if (rval == true) then
-				begin
-					Uslog.logf log_tag Uslog.Info "total vharness elements=%u\n"
-						(List.length uobjlib_vharness_list);
+		if (rval == false) then
+			begin
+				Uslog.logf log_tag Uslog.Error "invalid manifest hdr.";
+				ignore (exit 1);
+			end
+		;
 				
-					List.iter (fun x ->
-						Uslog.logf log_tag Uslog.Info "total elements=%u\n"
-							(List.length x);
+		if (compare usmf_hdr_type usmf_type_usuobj) <> 0 then
+			begin
+				Uslog.logf log_tag Uslog.Error "invalid uobj manifest type '%s'." !usmf_type;
+				ignore (exit 1);
+			end
+		;
+			
+		Uslog.logf log_tag Uslog.Info "Validated uobj hdr and manifest type.\n";
+						
+		let(rval, uobjlib_cfiles, uobjlib_casmfiles) = 
+			Usmanifest.parse_node_usmf_sources	mf_json in
 
-						List.iter (fun y ->
-							Uslog.logf log_tag Uslog.Info "element=%s\n" y;
-							()
-						) x;
+		if (rval == false) then
+			begin
+				Uslog.logf log_tag Uslog.Error "invalid usmf-sources node in manifest.";
+				ignore (exit 1);
+			end
+		;
 
-						()
-					) uobjlib_vharness_list;
+		Uslog.logf log_tag Uslog.Info "cfiles_count=%u, casmfiles_count=%u\n"
+					(List.length uobjlib_cfiles) (List.length uobjlib_casmfiles);
 
-				end
-			;
+
 
 																																										
-			Uslog.logf log_tag Uslog.Info "Done.\r\n";
+		Uslog.logf log_tag Uslog.Info "Done.\r\n";
 		()
 	;;
 								
