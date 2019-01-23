18efe79d1f4cfdf09980609e6e9f853d980add74
Amit Vasudevan <amitvasudevan@acm.org>
2019-01-22 19:22:32 -0600

    tools/libs: add usuobjgen to generate uobj support modules

---
+++ src/tools/libs/usuobjgen/usuobjgen.ml
@@ -0,0 +1,17 @@
+(*------------------------------------------------------------------------------
+	uberSpark uberobject support module generation interface
+	author: amit vasudevan (amitvasudevan@acm.org)
+------------------------------------------------------------------------------*)
+
+open Usconfig
+open Uslog
+open Usmanifest
+open Usextbinutils
+
+module Usuobjgen =
+	struct
+
+	let log_tag = "Usuobjgen";;
+
+																								
+	end
