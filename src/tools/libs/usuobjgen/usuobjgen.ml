65cd8874e2a4ee8452f6a16f2c107e8c3687e7f6
Amit Vasudevan <amitvasudevan@acm.org>
2019-02-18 18:52:26 -0600

    tools/libs: usuobjgen: refine interface generate_linker_script to sort memory regions and section definitions by increasing memory addresses

--- src/tools/libs/usuobjgen/usuobjgen.ml
+++ src/tools/libs/usuobjgen/usuobjgen.ml
@@ -13,7 +13,8 @@ module Usuobjgen =
 
 	let log_tag = "Usuobjgen";;
 
-
+	let hashtbl_keys h = Hashtbl.fold (fun key _ l -> key :: l) h [];;
+	
 	let generate_linker_script 
 		(fname : string)
 		(sections_hashtbl : (int, Usextbinutils.ld_section_info_t) Hashtbl.t)
@@ -30,13 +31,17 @@ module Usuobjgen =
 			Printf.fprintf oc "\nMEMORY";
 			Printf.fprintf oc "\n{";
 	
-			Hashtbl.iter (fun key (x:Usextbinutils.ld_section_info_t)  ->
-				(* new section memory *)
-				Printf.fprintf oc "\n %s (%s) : ORIGIN = 0x%08x, LENGTH = 0x%08x"
-					("mem_" ^ x.s_name)
-					( x.s_attribute ^ "ail") (x.s_origin) (x.s_length);
-			)  sections_hashtbl;
-					
+			let keys = List.sort compare (hashtbl_keys sections_hashtbl) in				
+			List.iter (fun key ->
+			    let x = Hashtbl.find sections_hashtbl key in
+					(* new section memory *)
+					Printf.fprintf oc "\n %s (%s) : ORIGIN = 0x%08x, LENGTH = 0x%08x"
+						("mem_" ^ x.s_name)
+						( x.s_attribute ^ "ail") (x.s_origin) (x.s_length);
+					()
+			) keys ;
+												
+																				
 			Printf.fprintf oc "\n}";
 			Printf.fprintf oc "\n";
 			
@@ -44,19 +49,21 @@ module Usuobjgen =
 			Printf.fprintf oc "\n{";
 			Printf.fprintf oc "\n";
 	
-			
-			Hashtbl.iter (fun key (x:Usextbinutils.ld_section_info_t)  ->
-				(* new section *)
-				Printf.fprintf oc "\n	. = 0x%08x;" x.s_origin;
-		    Printf.fprintf oc "\n %s : {" x.s_name;
-				List.iter (fun subsection ->
-					    Printf.fprintf oc "\n *(%s)" subsection;
-				) x.s_subsection_list;
-				Printf.fprintf oc "\n . = 0x%08x;" x.s_length; 
-		    Printf.fprintf oc "\n	} >mem_%s =0x9090" x.s_name;
-		    Printf.fprintf oc "\n";
-			) sections_hashtbl;
-	
+			let keys = List.sort compare (hashtbl_keys sections_hashtbl) in				
+			List.iter (fun key ->
+			    let x = Hashtbl.find sections_hashtbl key in
+					(* new section *)
+					Printf.fprintf oc "\n	. = 0x%08x;" x.s_origin;
+			    Printf.fprintf oc "\n %s : {" x.s_name;
+					List.iter (fun subsection ->
+						    Printf.fprintf oc "\n *(%s)" subsection;
+					) x.s_subsection_list;
+					Printf.fprintf oc "\n . = 0x%08x;" x.s_length; 
+			    Printf.fprintf oc "\n	} >mem_%s =0x9090" x.s_name;
+			    Printf.fprintf oc "\n";
+					()
+			) keys ;
+						
 			Printf.fprintf oc "\n";
 			Printf.fprintf oc "\n	/* this is to cause the link to fail if there is";
 			Printf.fprintf oc "\n	* anything we didn't explicitly place.";
