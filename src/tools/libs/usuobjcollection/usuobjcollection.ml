15818a925cf9756b45daee95af5117a2e85ed0c1
Amit Vasudevan <amitvasudevan@acm.org>
2019-03-07 12:24:33 -0600

    tools/libs: usuobjcoll: prepare to copy uobjcoll binary image within interface install

--- src/tools/libs/usuobjcollection/usuobjcollection.ml
+++ src/tools/libs/usuobjcollection/usuobjcollection.ml
@@ -413,6 +413,10 @@ module Usuobjcollection =
 		Usosservices.file_copy !usmf_filename_canonical 
 			(uobjcoll_install_dir ^ "/" ^ Usconfig.default_uobjcoll_usmf_name);
 		
+		(* copy uobj collection binary image *)
+		(* Usosservices.file_copy !usmf_filename_canonical 
+			(uobjcoll_install_dir ^ "/" ^ Usconfig.default_uobjcoll_usmf_name);
+			*)
 		
 		(* iterate over all the uobjs in the collection *)
 		(* and invoke their install method *)
