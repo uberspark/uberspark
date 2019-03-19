ecb3e94205106c666e90ef46e85d7b40677418a3
Amit Vasudevan <amitvasudevan@acm.org>
2019-03-19 15:14:51 -0500

    buildshims: refine gnumake build shim to export uberSpark CFLAGS and LFLAGS

--- src/buildshims/gnumake/uberspark.mk
+++ src/buildshims/gnumake/uberspark.mk
@@ -3,3 +3,29 @@
 # author: amit vasudevan (amitvasudevan@acm.org)
 ######
 
+######
+# input variables
+# UBERSPARK_UOBJCOLL = uobj collection name
+# UBERSPARK_UOBJ = uobj name
+######
+
+__UBERSPARK_INCLUDES = -I$(shell uberspark --info --get-includedir)
+__UBERSPARK_INCLUDES += -I$(shell ubersparkconfig --print-uberspark-hwmincludedir)
+__UBERSPARK_INCLUDES += -I$(shell ubersparkconfig --print-uberspark-loadersincludesdir)
+__UBERSPARK_INCLUDES += -I$(shell uberspark --info --uobjcoll $(UBERSPARK_UOBJCOLL) --uobj $(UBERSPARK_UOBJ) --get-includedir)
+
+
+UBERSPARK_CFLAGS = $(__UBERSPARK_INCLUDES)
+UBERSPARK_CFLAGS += $(shell ubersparkconfig --print-uberspark-basedefs)
+
+
+__UBERSPARK_LIBS_DIRS = $(shell ubersparkconfig --print-uberspark-loadersdir)
+
+
+UBERSPARK_LFLAGS = -Wl,-L,$(shell ubersparkconfig --print-uberspark-loadersdir)
+UBERSPARK_LFLAGS += -Wl,-l,usloader-linux-um
+UBERSPARK_LFLAGS += -Wl,-L,$(shell uberspark --info --uobjcoll app-fviews --uobj uobj_afvsensitive --get-libdir)
+UBERSPARK_LFLAGS += -Wl,-l,$(shell uberspark --info --uobjcoll app-fviews --uobj uobj_afvsensitive --arch x86_32 --get-libsentinels)
+UBERSPARK_LFLAGS += $(shell cat $(__UBERSPARK_LIBS_DIRS)/libusloader-linux-um.a.deps)
+
+
