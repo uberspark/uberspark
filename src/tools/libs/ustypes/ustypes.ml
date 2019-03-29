20b6b4cb0b3b0347c455559263ad8700715a753d
Amit Vasudevan <amitvasudevan@acm.org>
2019-03-29 13:20:44 -0500

    tools/libs: add module ustypes to house base type definitions

---
+++ src/tools/libs/ustypes/ustypes.ml
@@ -0,0 +1,15 @@
+(*
+	uberSpark type definitions
+	author: amit vasudevan (amitvasudevan@acm.org)
+*)
+
+module Ustypes =
+struct
+
+	type testtype_t = 
+		{
+			s_f1: string;
+			s_f2: int;
+		};;
+				
+end
