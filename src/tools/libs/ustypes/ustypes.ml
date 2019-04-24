d4a8aab3a5571e37f234ceba57c1345568b83437
Amit Vasudevan <amitvasudevan@acm.org>
2019-04-24 14:23:53 -0500

    tools/libs: ustypes: define usbinformat_section_info_t per binary format header and local section_info_t that encapsulates this with section name and subsection name list

--- src/tools/libs/ustypes/ustypes.ml
+++ src/tools/libs/ustypes/ustypes.ml
@@ -6,6 +6,29 @@
 module Ustypes =
 struct
 
+		(* from usbinformat.h *)
+		type usbinformat_section_info_t =
+			{
+				f_type         : int;			
+				f_prot         : int;			
+				f_va_offset    : int;
+				f_file_offset  : int;
+				f_size         : int;
+        f_aligned_at   : int;
+	      f_pad_to       : int;
+        f_reserved     : int; 
+			};;
+
+
+		(* local type definitions *)
+		type section_info_t = 
+			{
+				f_name: string;
+				f_subsection_list : string list;
+				usbinformat : usbinformat_section_info_t;
+			};;
+		
+
 		type uobjcoll_sentineltypes_t = 
 			{
 				s_type: string;
