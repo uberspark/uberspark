298835bb543ab12b4e49995cf419b8a55f4dff46
Amit Vasudevan <amitvasudevan@acm.org>
2019-09-07 23:40:58 -0500

    docs: revise conf.py to generate coarse-grained table of contents with alabaster theme as default html theme

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,10 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
 
+html_sidebars = {
+   '**': ['globaltoc.html', 'searchbox.html'],
+   'using/windows': ['windowssidebar.html', 'searchbox.html'],
+}
 
 
 # -- Options for LaTeX output ---------------------------------------------
