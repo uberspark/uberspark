23a488854a715ad779b4578e596be2ba8814cc7c
Amit Vasudevan <amitvasudevan@acm.org>
2019-09-11 00:03:21 -0500

    build-trusses: revise build-docs.mk to install documentation within docs/_build

--- build-trusses/build-docs.mk
+++ build-trusses/build-docs.mk
@@ -10,21 +10,20 @@ include ./commondefs.mk
 ### common documentation build prep
 .PHONY: docs_build_prep
 docs_build_prep:
-	mkdir -p $(UBERSPARK_BUILDDIR)
-	mkdir -p $(UBERSPARK_BUILDDIR)/docs
+	mkdir -p $(UBERSPARK_DOCSDIR)/_build
 
 ### generate .pdf documentation
 .PHONY: docs_pdf
 docs_pdf: docs_clean docs_build_prep
-	sphinx-build -b latex "$(UBERSPARK_DOCSDIR)" "$(UBERSPARK_BUILDDIR)"/docs
+	sphinx-build -b latex "$(UBERSPARK_DOCSDIR)" "$(UBERSPARK_DOCSDIR)"/docs
 	@cd "$(UBERSPARK_BUILDDIR)"/docs && make 
 
 ### generate .html documentation
 .PHONY: docs_html
 docs_html: docs_clean docs_build_prep
-	sphinx-build -b html "$(UBERSPARK_DOCSDIR)" "$(UBERSPARK_BUILDDIR)"/docs
+	sphinx-build -b html "$(UBERSPARK_DOCSDIR)" "$(UBERSPARK_DOCSDIR)"/docs
 
 ### cleanup documentation build
 .PHONY: docs_clean
 docs_clean:
-	rm -rf $(UBERSPARK_BUILDDIR)/docs
+	mkdir -p $(UBERSPARK_DOCSDIR)/_build
