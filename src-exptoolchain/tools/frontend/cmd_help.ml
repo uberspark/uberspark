8c2e3619f79cf8a4a002c75cb7a24270cd7bfc2e
Amit Vasudevan <amitvasudevan@acm.org>
2019-09-13 22:03:47 -0500

    src-exptoolchain/tools/frontend: refine help command handler to pretty print list of commands

--- src-exptoolchain/tools/frontend/cmd_help.ml
+++ src-exptoolchain/tools/frontend/cmd_help.ml
@@ -1,19 +1,28 @@
 (* uberspark front-end command processing logic for command: help *)
 (* author: amit vasudevan (amitvasudevan@acm.org) *)
 
+open Uslog
 open Cmdliner
 
 (* handler for help command *)
-let handler_help copts man_format cmds topic = match topic with
-| None -> `Help (`Pager, None) (* help about the program. *)
-| Some topic ->
-		(* let topics = "topics" :: "patterns" :: "environment" :: cmds in *)
-		let topics = "topics" :: cmds in 
-		let conv, _ = Cmdliner.Arg.enum (List.rev_map (fun s -> (s, s)) topics) in
-    match conv topic with
-    | `Error e -> `Error (false, e)
-    | `Ok t when t = "topics" -> List.iter print_endline cmds; `Ok ()
-    | `Ok t when List.mem t cmds -> `Help (man_format, Some t)
-    | `Ok t ->
-        let page = (topic, 7, "", "", ""), [`S topic; `P "Say something";] in
-        `Ok (Cmdliner.Manpage.print man_format Format.std_formatter page)
+let handler_help 
+  (copts : Commonopts.opts)
+  man_format cmds topic = 
+  Commoninit.initialize copts;
+  match topic with
+    | None -> `Help (`Pager, None) (* help about the program. *)
+    | Some topic ->
+		  let topics = "topics" :: cmds in 
+		  let conv, _ = Cmdliner.Arg.enum (List.rev_map (fun s -> (s, s)) topics) in
+      match conv topic with
+        | `Error e -> `Error (false, e)
+        | `Ok t when t = "topics" -> Uslog.log "help is available for the following COMMANDs:";
+                                      List.iter (fun x ->
+                                          Uslog.log "%s" x;
+                                      )  cmds; 
+                                      Uslog.log "use uberspark help COMMAND or uberspark COMMAND --help";
+                                      `Ok ()
+        | `Ok t when List.mem t cmds -> `Help (man_format, Some t)
+        | `Ok t ->
+          let page = (topic, 7, "", "", ""), [`S topic; `P "Cowabunga!";] in
+          `Ok (Cmdliner.Manpage.print man_format Format.std_formatter page)
