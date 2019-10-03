4176dfe3e204a8cb7d20784bd83ba103c824bfa7
Amit Vasudevan <amitvasudevan@acm.org>
2019-10-03 09:37:51 -0500

    src-nextgen/tools: successful build of frontend

--- src-nextgen/tools/libs/uberspark.ml
+++ src-nextgen/tools/libs/uberspark.ml
@@ -9,16 +9,3 @@ module Manifest = Uberspark_manifest
 module Binary = Uberspark_binary
 module Uobj = Uberspark_uobj
 
-(*
-module Usuobj =
-struct
-
-		type sentinel_info_t = 
-			{
-				s_type: string;
-      };;
-    
-    let my_method = Uberspark_log.log "hello there";
-    
-    end
-*)
