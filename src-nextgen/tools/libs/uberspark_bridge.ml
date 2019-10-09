c42372d386cb5fff68d73c1112a0c94a048ee420
Amit Vasudevan <amitvasudevan@acm.org>
2019-10-09 11:35:23 -0500

    src-nextgen/tools/libs: add bridge header and cc-bridge types to module bridge...

--- src-nextgen/tools/libs/uberspark_bridge.ml
+++ src-nextgen/tools/libs/uberspark_bridge.ml
@@ -4,3 +4,24 @@
 *)
 open Unix
 open Yojson
+
+type bridge_hdr_t = {
+	mutable btype : string;
+	mutable execname: string;
+	mutable path: string;
+	mutable params: string list;
+	mutable extexecname: string;
+	mutable devenv: string;
+	mutable arch: string;
+	mutable cpu: string;
+	mutable version: string;
+	mutable namespace: string;
+}
+;;
+
+type cc_bridge_t = { 
+	mutable hdr : bridge_hdr_t;
+	mutable params_prefix_to_obj: string list;
+	mutable params_prefix_to_asm: string list;
+	mutable params_prefix_to_output: string list;
+};;
