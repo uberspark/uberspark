de4e4fd1406420d66e6f576fb02d638d014bc4fe
Amit Vasudevan <amitvasudevan@acm.org>
2019-10-28 14:15:00 -0500

    src-nextgen/tools/libs: manifest.bridge: add bridge manifest node data types

--- src-nextgen/tools/libs/uberspark_manifest.mli
+++ src-nextgen/tools/libs/uberspark_manifest.mli
@@ -40,6 +40,40 @@ val write_epilogue : ?epilogue_str:string -> out_channel -> bool
 (* submodules *)
 (****************************************************************************)
 
+
+module Bridge : sig
+
+  (****************************************************************************)
+  (* manifest node types *)
+  (****************************************************************************)
+
+  (* bridge-hdr node type *)
+  type bridge_hdr_t = {
+    mutable btype : string;
+    mutable execname: string;
+    mutable devenv: string;
+    mutable arch: string;
+    mutable cpu: string;
+    mutable version: string;
+    mutable path: string;
+    mutable params: string list;
+    mutable container_fname: string;
+    mutable namespace: string;
+  }
+
+  (* bridge-cc node type *)
+  type bridge_cc_t = { 
+    mutable bridge_hdr : bridge_hdr_t;
+    mutable params_prefix_to_obj: string;
+    mutable params_prefix_to_asm: string;
+    mutable params_prefix_to_output: string;
+  }
+
+
+end
+
+
+
 module Config : sig
 
   (****************************************************************************)
--- src-nextgen/tools/libs/uberspark_manifest_bridge.ml
+++ src-nextgen/tools/libs/uberspark_manifest_bridge.ml
@@ -11,9 +11,27 @@
 (****************************************************************************)
 
 (* bridge-hdr node type *)
-
+type bridge_hdr_t = {
+	mutable btype : string;
+	mutable execname: string;
+	mutable devenv: string;
+	mutable arch: string;
+	mutable cpu: string;
+	mutable version: string;
+	mutable path: string;
+	mutable params: string list;
+	mutable container_fname: string;
+	mutable namespace: string;
+}
+;;
 
 (* bridge-cc node type *)
+type bridge_cc_t = { 
+	mutable bridge_hdr : bridge_hdr_t;
+	mutable params_prefix_to_obj: string;
+	mutable params_prefix_to_asm: string;
+	mutable params_prefix_to_output: string;
+};;
 
 
 (****************************************************************************)
