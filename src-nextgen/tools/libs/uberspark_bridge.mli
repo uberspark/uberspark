83af580c1fae49976af8180e7e54e521f1b1103f
Amit Vasudevan <amitvasudevan@acm.org>
2019-11-01 09:44:35 -0500

    src-nextgen/tools/libs: bridge: cleanup now defunct code,data and types

--- src-nextgen/tools/libs/uberspark_bridge.mli
+++ src-nextgen/tools/libs/uberspark_bridge.mli
@@ -3,31 +3,6 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 *)
 
-(*
-type bridge_hdr_t = {
-	mutable btype : string;
-	mutable execname: string;
-	mutable path: string;
-	mutable params: string list;
-	mutable container_fname: string;
-	mutable devenv: string;
-	mutable arch: string;
-	mutable cpu: string;
-	mutable version: string;
-	mutable namespace: string;
-}
-
-type cc_bridge_t = { 
-	mutable hdr : bridge_hdr_t;
-	mutable params_prefix_to_obj: string;
-	mutable params_prefix_to_asm: string;
-	mutable params_prefix_to_output: string;
-}
-
-
-val cc_bridge_settings_loaded : bool ref
-*)
-
 val bridge_cc : Uberspark_manifest.Bridge.bridge_cc_t 
 
 module Container : sig
@@ -52,8 +27,6 @@ val store_bridge_cc_to_file : string -> bool
 val store_bridge_cc : unit -> bool
 
 val build_bridge_cc : unit -> bool
-
-val build: string list -> unit
 
 val dump : string -> ?bridge_exectype:string -> string -> unit
 
