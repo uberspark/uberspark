4365edff2ea514a8e22591d1ab27e29b9e999a1d
Amit Vasudevan <amitvasudevan@acm.org>
2019-11-18 09:58:35 -0600

    src-nextgen/tools/libs: add module uberspark_uobjcoll to house uobj collection related interfaces

---
+++ src-nextgen/tools/libs/uberspark_uobjcoll.ml
@@ -0,0 +1,19 @@
+(*------------------------------------------------------------------------------
+	uberSpark uberobject collection verification and build interface implementation
+	author: amit vasudevan (amitvasudevan@acm.org)
+------------------------------------------------------------------------------*)
+
+open Str
+
+
+
+
+
+let build
+	(target_def : Defs.Basedefs.target_def_t)
+	: bool =
+
+	let retval = ref false in
+
+	(!retval)
+;;
---
+++ src-nextgen/tools/libs/uberspark_uobjcoll.mli
@@ -0,0 +1,6 @@
+(*------------------------------------------------------------------------------
+	uberSpark uberobject collection verification and build interface
+	author: amit vasudevan (amitvasudevan@acm.org)
+------------------------------------------------------------------------------*)
+
+val build : Defs.Basedefs.target_def_t -> bool
