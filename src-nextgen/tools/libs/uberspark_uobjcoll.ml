2e85015c7b891643ef49c45b2e9ee0b04bd5d480
Amit Vasudevan <amitvasudevan@acm.org>
2019-11-18 10:45:05 -0600

    src-nextgen/tools/libs: uberspark.uobjcoll: add interface parse_manifest to parse uobjcoll manifest; currently just the header...

--- src-nextgen/tools/libs/uberspark_uobjcoll.ml
+++ src-nextgen/tools/libs/uberspark_uobjcoll.ml
@@ -6,6 +6,50 @@
 open Str
 
 
+let d_mf_filename = ref "";;
+let get_d_mf_filename = !d_mf_filename;;
+
+let d_path_to_mf_filename = ref "";;
+let get_d_path_to_mf_filename = !d_path_to_mf_filename;;
+
+let d_path_ns = ref "";;
+let get_d_path_ns = !d_path_ns;;
+
+let d_hdr: Uberspark_manifest.Uobjcoll.uobjcoll_hdr_t = {f_namespace = ""; f_platform = ""; f_arch = ""; f_cpu = ""};;
+let get_d_hdr = d_hdr;;
+
+
+(*--------------------------------------------------------------------------*)
+(* parse uobjcoll manifest *)
+(* uobjcoll_mf_filename = uobj collection manifest filename *)
+(*--------------------------------------------------------------------------*)
+let parse_manifest 
+	(uobjcoll_mf_filename : string)
+	: bool =
+	
+	(* store filename and uobjcoll path to filename *)
+	d_mf_filename := Filename.basename uobjcoll_mf_filename;
+	d_path_to_mf_filename := Filename.dirname uobjcoll_mf_filename;
+	
+	(* read manifest JSON *)
+	let (rval, mf_json) = Uberspark_manifest.get_manifest_json get_d_mf_filename in
+	
+	if (rval == false) then (false)
+	else
+
+	(* parse uobjcoll-hdr node *)
+	let rval = (Uberspark_manifest.Uobjcoll.parse_uobjcoll_hdr mf_json d_hdr ) in
+	if (rval == false) then (false)
+	else
+
+	let dummy=0 in begin
+		d_path_ns := !Uberspark_namespace.namespace_root_dir  ^ "/" ^ d_hdr.f_namespace;
+		Uberspark_logger.log ~lvl:Uberspark_logger.Debug "uobj collection path ns=%s" get_d_path_ns;
+	end;
+
+	(true)
+;;
+
 
 
 
--- src-nextgen/tools/libs/uberspark_uobjcoll.mli
+++ src-nextgen/tools/libs/uberspark_uobjcoll.mli
@@ -3,4 +3,5 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 ------------------------------------------------------------------------------*)
 
+val parse_manifest : string -> bool
 val build : Defs.Basedefs.target_def_t -> bool
