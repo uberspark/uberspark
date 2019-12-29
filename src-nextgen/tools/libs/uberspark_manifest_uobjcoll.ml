7cb8385be08b4cd6922d7debb848bf1d852a14e2
Amit Vasudevan <amitvasudevan@acm.org>
2019-12-28 23:44:08 -0600

    src-nextgen/tools/libs: add uobjcoll-interuobjcoll-publicmethods manifest node within uobjcoll manifest; add supporting parsing interfaces for hashtbl and assoc list

--- src-nextgen/tools/libs/uberspark_manifest_uobjcoll.ml
+++ src-nextgen/tools/libs/uberspark_manifest_uobjcoll.ml
@@ -18,6 +18,15 @@ type uobjcoll_uobjs_t =
 	mutable f_templar_uobjs    : string list;
 };;
 
+
+type uobjcoll_interuobjcoll_publicmethods_t =
+{
+	mutable f_uobj_ns    : string;
+	mutable f_pm_name	 : string;
+	mutable f_sentinel_type_list : string list;
+};;
+
+
 type uobjcoll_sentinels_t =
 {
 	mutable f_interuobjcoll    : string list;
@@ -87,6 +96,111 @@ let parse_uobjcoll_uobjs
 			retval := false;
 	;
 
+	(!retval)
+;;
+
+
+
+(*--------------------------------------------------------------------------*)
+(* parse manifest json node "uobjcoll-interuobjcoll-publicmethods" into hashtbl *)
+(* return: *)
+(* on success: true; hash table modified with parsed values *)
+(* on failure: false; hash table left untouched *)
+(*--------------------------------------------------------------------------*)
+let parse_uobjcoll_interuobjcoll_publicmethods_into_hashtbl
+	(mf_json : Yojson.Basic.t)
+	(publicmethods_hashtbl : ((string, uobjcoll_interuobjcoll_publicmethods_t)  Hashtbl.t))
+	: bool =
+		
+	let retval = ref false in
+
+	try
+		let open Yojson.Basic.Util in
+			let uobjcoll_interuobjcoll_publicmethods_json = mf_json |> member "uobjcoll-interuobjcoll-publicmethods" in
+				if uobjcoll_interuobjcoll_publicmethods_json != `Null then
+					begin
+
+						let uobj_ns_assoc_list = Yojson.Basic.Util.to_assoc uobjcoll_interuobjcoll_publicmethods_json in
+							
+						List.iter (fun ( (uobj_ns:string),(uobj_ns_json:Yojson.Basic.t)) ->
+
+							let pm_name_assoc_list = Yojson.Basic.Util.to_assoc uobj_ns_json in
+
+							List.iter (fun ( (pm_name:string),(sentinel_type_list_json:Yojson.Basic.t)) ->
+								let sentinel_type_list = (json_list_to_string_list  (sentinel_type_list_json |> to_list)) in
+								let entry : uobjcoll_interuobjcoll_publicmethods_t = {
+									f_uobj_ns = uobj_ns;
+									f_pm_name = pm_name;
+									f_sentinel_type_list = sentinel_type_list;
+								} in
+								let canonical_pm_name_key = uobj_ns ^ "__" ^ pm_name in
+								Hashtbl.add publicmethods_hashtbl canonical_pm_name_key entry;
+
+							) pm_name_assoc_list;
+
+						) uobj_ns_assoc_list;
+
+						retval := true;
+					end
+				;
+														
+	with Yojson.Basic.Util.Type_error _ -> 
+			retval := false;
+	;
+
+							
+	(!retval)
+;;
+
+
+(*--------------------------------------------------------------------------*)
+(* parse manifest json node "uobjcoll-interuobjcoll-publicmethods" into assoc list *)
+(* return: *)
+(* on success: true; assoc list is modified with parsed values *)
+(* on failure: false; assoc list is left untouched *)
+(*--------------------------------------------------------------------------*)
+let parse_uobjcoll_interuobjcoll_publicmethods
+	(mf_json : Yojson.Basic.t)
+	(publicmethods_assoc_list : (string * uobjcoll_interuobjcoll_publicmethods_t) list ref)
+	: bool =
+		
+	let retval = ref false in
+
+	try
+		let open Yojson.Basic.Util in
+			let uobjcoll_interuobjcoll_publicmethods_json = mf_json |> member "uobjcoll-interuobjcoll-publicmethods" in
+				if uobjcoll_interuobjcoll_publicmethods_json != `Null then
+					begin
+
+						let uobj_ns_assoc_list = Yojson.Basic.Util.to_assoc uobjcoll_interuobjcoll_publicmethods_json in
+							
+						List.iter (fun ( (uobj_ns:string),(uobj_ns_json:Yojson.Basic.t)) ->
+
+							let pm_name_assoc_list = Yojson.Basic.Util.to_assoc uobj_ns_json in
+
+							List.iter (fun ( (pm_name:string),(sentinel_type_list_json:Yojson.Basic.t)) ->
+								let sentinel_type_list = (json_list_to_string_list  (sentinel_type_list_json |> to_list)) in
+								let entry : uobjcoll_interuobjcoll_publicmethods_t = {
+									f_uobj_ns = uobj_ns;
+									f_pm_name = pm_name;
+									f_sentinel_type_list = sentinel_type_list;
+								} in
+								let canonical_pm_name_key = uobj_ns ^ "__" ^ pm_name in
+								publicmethods_assoc_list := !publicmethods_assoc_list @ [ (canonical_pm_name_key, entry) ];
+
+							) pm_name_assoc_list;
+
+						) uobj_ns_assoc_list;
+
+						retval := true;
+					end
+				;
+														
+	with Yojson.Basic.Util.Type_error _ -> 
+			retval := false;
+	;
+
+							
 	(!retval)
 ;;
 
