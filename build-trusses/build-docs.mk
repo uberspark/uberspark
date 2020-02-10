03de2187f9a778e5ed4507f24ae7303ca7908781
Amit Vasudevan <amitvasudevan@acm.org>
2020-02-10 12:56:35 -0600

    build-trusses: refine build-docs.mk to plug in html/css theme in _temp folder docs: update conf.py to exclude _themes and _temp folders from doc build process

--- build-trusses/build-docs.mk
+++ build-trusses/build-docs.mk
@@ -10,10 +10,11 @@ include ./commondefs.mk
 ### common documentation build prep
 .PHONY: docs_build_prep
 docs_build_prep:
+	mkdir -p $(UBERSPARK_DOCSDIR)/_temp
 	mkdir -p $(UBERSPARK_DOCSDIR)/_build
 	mkdir -p $(UBERSPARK_DOCSDIR)/_themes
-	cd $(UBERSPARK_DOCSDIR)/_build && git clone https://github.com/hypcode/uberspark-htmlcss-themes.git uberspark-htmlcss-themes.git
-	cd $(UBERSPARK_DOCSDIR)/_themes && ln -sf ../_build/uberspark-htmlcss-themes.git/rtd_uberspark/sphinx_rtd_theme/ rtd_uberspark
+	cd $(UBERSPARK_DOCSDIR)/_temp && git clone https://github.com/hypcode/uberspark-htmlcss-themes.git uberspark-htmlcss-themes.git
+	cd $(UBERSPARK_DOCSDIR)/_themes && ln -sf ../_temp/uberspark-htmlcss-themes.git/rtd_uberspark/sphinx_rtd_theme/ rtd_uberspark
 
 
 ### generate .pdf documentation
@@ -32,3 +33,4 @@ docs_html: docs_clean docs_build_prep
 docs_clean:
 	rm -rf $(UBERSPARK_DOCSDIR)/_build
 	rm -rf $(UBERSPARK_DOCSDIR)/_themes
+	rm -rf $(UBERSPARK_DOCSDIR)/_temp
