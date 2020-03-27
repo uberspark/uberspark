67451fae40d965f3c917461dc27dd54f831a663f
Amit Vasudevan <amitvasudevan@acm.org>
2020-03-27 11:13:17 -0500

    nextgen-toolkit: coss-examples: hello-mul: bring in README and main.c within revision control

---
+++ src-nextgen/coss-examples/hello-mul/main.c
@@ -0,0 +1,14 @@
+/*
+    minimal coss example application that multiplies two integers 
+    specified via integer parameters and returns the result
+
+    author: amit vasudevan <amitvasudevan@acm.org>
+*/
+
+int main (int multiplicand, int multiplier){
+    int res_mul;
+
+    res_mul = multiplicand * multiplier;
+
+    return res_mul;
+}
