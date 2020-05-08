0dd73fe168e93d5bd6daa2627ac3b2025fb4ffc5
David Shepard <daveman1010220@gmail.com>
2020-05-08 16:20:42 -0400

    add remaining files for the containers build that are masked by the gitignore

---
+++ src-nextgen/bridges/cc-bridge/container/amd64/x86_32/generic/compcert/v3.3/compiler_script.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# This script is meant to be sourced from within the Dockerfile to pass in vars.
+
+echo "archive_name: $archive_name"
+
+archive_string_len=${#archive_name}
+
+echo "archive_string_len: $archive_string_len"
+
+archive_dir=$(echo $archive_name | awk -v var=$archive_string_len '{ string=substr($0, 1, var - 4); print string; }' ) &&\
+
+echo "archive_dir: $archive_dir"
+
+cd $archive_dir
+
+#find / -name 'ocaml'
+
+echo "PATH: $PATH"
+ls -alR /home/ubuntu/.opam
+
+su ubuntu
+
+echo "PATH before mod: $PATH"
+
+echo "PATH=/home/ubuntu/.opam/4.05.0/bin:$PATH" >> /home/ubuntu/.bashrc
+
+PATH=/home/ubuntu/.opam/4.05.0/bin:$PATH
+echo "PATH after mod: $PATH"
+
+./configure x86_32-linux
+make -j4 all
+exit
+make install
