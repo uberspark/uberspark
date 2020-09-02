fee3c7ca5c88b28acc965ca1540178bab5993f83
Amit Vasudevan <amitvasudevan@acm.org>
2020-09-01 19:13:40 -0500

    src-nextgen/tools/libs: revise uberspark.manifest.uobj.json_node_uberspark_uobj_sections_to_var to use new section definition rules within manifest

--- src-nextgen/tools/libs/uberspark_manifest_uobj.ml
+++ src-nextgen/tools/libs/uberspark_manifest_uobj.ml
@@ -340,58 +340,44 @@ let json_node_uberspark_uobj_sections_to_var
 	let retval = ref true in
 	let sections_assoc_list : (string * Defs.Basedefs.section_info_t) list ref = ref [] in
 
+
 	try
-	let open Yojson.Basic.Util in
+		let open Yojson.Basic.Util in
+			let uobj_sections_json = json_node_uberspark_uobj |> member "sections" in
+				if uobj_sections_json != `Null then
+					begin
 
-		let uobj_sections_json = json_node_uberspark_uobj |> member "sections" in
-			if uobj_sections_json != `Null then
-				begin
-					
-					let uobj_sections_assoc_list = Yojson.Basic.Util.to_assoc uobj_sections_json in
-						retval := true;
-						List.iter (fun (x,y) ->
-								(* x = section name, y = list of section attributes *)
-								let uobj_sections_attribute_list = (Yojson.Basic.Util.to_list y) in
-									if (List.length uobj_sections_attribute_list  < 6 ) then
-										begin
-											Uberspark_logger.log ~lvl:Uberspark_logger.Error "insufficient entries within section attribute list for section: %s" x;															retval := false;
-										end
-									else
-										begin
-											let subsection_list = ref [] in 
-											for index = 5 to ((List.length uobj_sections_attribute_list)-1) do 
-												subsection_list := !subsection_list @	[ ((List.nth uobj_sections_attribute_list index) |> to_string) ]
-											done;
+						let uobj_sections_list = Yojson.Basic.Util.to_list uobj_sections_json in
+							retval := true;
+							
+							List.iter (fun x ->
+								let section_entry : Defs.Basedefs.section_info_t = 
+								{ 
+									f_name = Yojson.Basic.Util.to_string (x |> member "name");	
+									f_subsection_list = [];	
+									usbinformat = { f_type = int_of_string (Yojson.Basic.Util.to_string (x |> member "type")); 
+													f_prot = int_of_string (Yojson.Basic.Util.to_string (x |> member "prot")); 
+													f_size = int_of_string (Yojson.Basic.Util.to_string (x |> member "size"));
+													f_aligned_at = int_of_string (Yojson.Basic.Util.to_string (x |> member "aligned_at")); 
+													f_pad_to = int_of_string (Yojson.Basic.Util.to_string (x |> member "pad_to")); 
+													f_addr_start=0; 
+													f_addr_file = 0;
+													f_reserved = 0;
+													};
+								} in
 
-											let section_entry : Defs.Basedefs.section_info_t = 
-											{ 
-												f_name = (x);	
-												f_subsection_list = !subsection_list;	
-												usbinformat = { f_type = int_of_string ((List.nth uobj_sections_attribute_list 0) |> to_string); 
-																				f_prot = int_of_string ((List.nth uobj_sections_attribute_list 1) |> to_string); 
-																				f_size = int_of_string ((List.nth uobj_sections_attribute_list 2) |> to_string);
-																				f_aligned_at = int_of_string ((List.nth uobj_sections_attribute_list 3) |> to_string); 
-																				f_pad_to = int_of_string ((List.nth uobj_sections_attribute_list 4) |> to_string); 
-																				f_addr_start=0; 
-																				f_addr_file = 0;
-																				f_reserved = 0;
-																			};
-											} in
-				
-											sections_assoc_list := !sections_assoc_list @ [ (x, section_entry) ]; 
-											
-											retval := true;
-										end
-									;
+								sections_assoc_list := !sections_assoc_list @ [ (section_entry.f_name, section_entry) ];
+												
 								()
-							) uobj_sections_assoc_list;
-				end
-			;		
-			
-													
+							) uobj_sections_list;
+
+					end
+				;
+														
 	with Yojson.Basic.Util.Type_error _ -> 
-		retval := false;
+			retval := false;
 	;
+
 
 						
 	(!retval, !sections_assoc_list)
