ec3811e1b36dca17163c5f5283f86582e7030052
Amit Vasudevan <amitvasudevan@acm.org>
2020-09-30 13:17:18 -0500

    build-trusses: revise installation target to install verification bridge plugin under tools/vbridge-plugin

--- build-trusses/install.mk
+++ build-trusses/install.mk
@@ -21,6 +21,8 @@ install_createnamespace:
 	rm -rf $(UBERSPARK_INSTALLDIR)
 	mkdir -p $(UBERSPARK_INSTALLDIR)
 	mkdir -p $(UBERSPARK_INSTALLDIR)/bin
+	mkdir -p $(UBERSPARK_INSTALLDIR)/tools
+	mkdir -p $(UBERSPARK_INSTALLDIR)/tools/vbridge-plugin
 	mkdir -p $(UBERSPARK_INSTALLDIR)/bridges
 	mkdir -p $(UBERSPARK_INSTALLDIR)/docs
 	mkdir -p $(UBERSPARK_INSTALLDIR)/platforms
@@ -52,6 +54,7 @@ install_populateamespace_include:
 install_populateamespace: install_populateamespace_include
 	@echo Populating namespace within: $(UBERSPARK_INSTALLDIR)...
 	cp -rf $(UBERSPARK_SRCDIR)/bridges/* $(UBERSPARK_INSTALLDIR)/bridges/
+	cp -f $(UBERSPARK_SRCDIR)/tools/vbridge-plugin/top/* $(UBERSPARK_INSTALLDIR)/tools/vbridge-plugin/.
 	cp -rf $(UBERSPARK_DOCSDIR)/_build/* $(UBERSPARK_INSTALLDIR)/docs/
 	cp -rf $(UBERSPARK_SRCDIR)/platforms/* $(UBERSPARK_INSTALLDIR)/platforms/
 	cp -rf $(UBERSPARK_SRCDIR)/config/* $(UBERSPARK_INSTALLDIR)/staging/config/
