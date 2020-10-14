daed89f9e959db72ec5c8a95341a4288c4d6ff0d
Amit Vasudevan <amitvasudevan@acm.org>
2020-10-14 18:54:19 -0500

    src-nextgen/coss-examples/hello-mul: revise ucoss-src/uobjcoll/main.c to invoke main_nullfunc and change main_nullfunc prototype to use the canonical void * parameter in case of no input parameters; successful build

--- src-nextgen/coss-examples/hello-mul/ucoss-src/uobjcoll/main/main.c
+++ src-nextgen/coss-examples/hello-mul/ucoss-src/uobjcoll/main/main.c
@@ -9,11 +9,13 @@
 #include <uberspark/include/uberspark.h>
 #include <uberspark/hwm/include/arch/x86_32/generic/hwm.h>
 
-CASM_FUNCDECL(void main_nullfunc(void));
+CASM_FUNCDECL(void main_nullfunc(void *noparam));
 
 uint32_t main (uint32_t multiplicand, uint32_t multiplier){
     uint32_t result;
 
+    CASM_FUNCCALL(main_nullfunc, CASM_NOPARAM);
+    
     result = multiplicand * multiplier;
 
     return result;
