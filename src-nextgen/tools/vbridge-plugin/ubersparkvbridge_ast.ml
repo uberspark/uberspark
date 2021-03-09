e1fa56d81864aa16783c8a34559ead8b70f3296e
Amit Vasudevan <amitvasudevan@acm.org>
2021-03-08 18:36:58 -0600

    tools/vbridge: gather and print all global function definitions; only those that haev a full body definition witin the sources

--- src-nextgen/tools/vbridge-plugin/ubersparkvbridge_ast.ml
+++ src-nextgen/tools/vbridge-plugin/ubersparkvbridge_ast.ml
@@ -7,7 +7,37 @@
 open Cil_datatype
 open Cil_types
    		
-    		
+(* see section 4.17 in plugin development guide as well as
+frama-c-api/html/Cil.html 
+
+also see frama-c-api/html/Cil_types.html for the type; each method! name corresponds to a type
+but not always same name; e.g., the vfunc method is for type funcdec*)
+class ast_visitor = object(self)
+  inherit Visitor.frama_c_inplace
+
+  method! vfunc fdec =
+    Ubersparkvbridge_print.output (Printf.sprintf "global defined function: %s" fdec.svar.vname);
+
+    (Cil.DoChildren)
+  ;
+
+end;;
+
+
+(* gather all global function definitions *)
+let ast_get_global_function_definitions (p_ast_file :  Cil_types.file) : unit =
+
+  let l_visitor = new ast_visitor in
+  
+  Visitor.visitFramacFileSameGlobals (l_visitor:>Visitor.frama_c_visitor) p_ast_file;
+
+;;
+
+
+
+
+
+
 (* dump AST of the source files provided *)    		
 let ast_dump 
     ()
@@ -19,13 +49,15 @@ let ast_dump
     let file = Ast.get () in
 
     (* pretty print it *)
-    Kernel.CodeOutput.output (fun fmt -> Printer.pp_file fmt file);
-    (*Printer.pp_file Format.std_formatter file;*)
+    (* see frama-c-api/html/Printer_api.S_pp.html for Printer.pp_file documentation *)
+    (*Kernel.CodeOutput.output (fun fmt -> Printer.pp_file fmt file);*)
+    Printer.pp_file Format.std_formatter file;
 
+    ast_get_global_function_definitions file;
 
 		Ubersparkvbridge_print.output "AST dump Done.\n";
 		()
-
+;;
 
 
 
