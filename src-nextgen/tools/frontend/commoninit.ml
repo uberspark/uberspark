cc89fbbaeb0a4602c84017465fb9484c1000f638
Amit Vasudevan <amitvasudevan@acm.org>
2021-05-03 12:34:05 -0500

    tools/frontend: add interface check_for_manifest implementation to check if a manifest is present in the directory the cli is invoked...

--- src-nextgen/tools/frontend/commoninit.ml
+++ src-nextgen/tools/frontend/commoninit.ml
@@ -4,7 +4,52 @@
 open Uberspark
 open Cmdliner
 
-let initialize 
+(*
+  returns (is_manifest_present, is_error)
+  (true, false) => manifest present and no error
+  (false, false) => manifest not present and no error
+  (false, true) => manifest not present and error
+*)
+let check_for_manifest 
+  (copts : Commonopts.opts) : (bool * bool) = 
+  
+  (* setup logging level as specified in the cli and kick-start execution *)
+  Uberspark.Logger.current_level := copts.log_level;
+  Uberspark.Logger.log ~lvl:Uberspark.Logger.Debug "check_for_manifest...";
+
+  (* get current working directory *)
+  let l_cwd = Uberspark.Osservices.getcurdir() in
+
+  (* get the absolute path of the current working directory *)
+  let (rval, l_cwd_abs) = (Uberspark.Osservices.abspath l_cwd) in
+
+  (* bail out on error *)
+  if (rval == false) then
+    Uberspark.Logger.log ~lvl:Uberspark.Logger.Error "check_for_manifest: could not get absolute path of current working directory!";
+    (false, true)
+  else
+
+  (* announce working directory *)
+  let l_dummy=0 in begin
+  Uberspark.Logger.log ~lvl:Uberspark.Logger.Debug "check_for_manifest: current working directory: %s" l_cwd_abs;
+  end;
+
+  (* check if manifest exists within current working directory *)
+  let l_manifest_file_path_abs = (l_cwd_abs ^ "/" ^ Uberspark.Namespace.namespace_root_mf_filename) in
+  let rval = (Uberspark.Osservices.file_exists l_manifest_file_path_abs) in
+
+  (* if not, display cli help and exit *)
+  if (rval == false) then
+    (false, false)
+  else
+
+  (true, false)
+;;
+
+
+
+
+let initialize_context 
   (copts : Commonopts.opts) = 
   
   Uberspark.Context.initialize ~p_log_level:copts.log_level
@@ -16,7 +61,6 @@ let initialize
       "creator: amit vasudevan <amitvasudevan@acm.org>";
       "";
     ];
-
 
 ;;
 
