83997787517bdae2ebb1cc513b8ef0ab46391c3b
Amit Vasudevan <amitvasudevan@acm.org>
2021-06-28 14:15:56 -0500

    tools/frontend: revise Cmd_verify.handler_verify to populate verify options association list

--- src-nextgen/tools/frontend/cmd_verify.ml
+++ src-nextgen/tools/frontend/cmd_verify.ml
@@ -5,15 +5,19 @@ open Uberspark
 open Cmdliner
 
 type opts = { 
+  uobject : string option;
   baseinv : bool; 
 };;
 
 (* fold all verify options into type opts *)
 let cmd_verify_opts_handler 
+  (p_uobject : string option)
   (p_baseinv : bool)
   : opts = 
   
-  { baseinv = p_baseinv; }
+  { 
+    uobject = p_uobject; 
+    baseinv = p_baseinv; }
 ;;
 
 
@@ -21,12 +25,19 @@ let cmd_verify_opts_handler
 let cmd_verify_opts_t =
   let docs = "VERIFY OPTIONS" in
 
+  let uobject =
+    let doc = "Perform verification only for uobject specified by $(docv)."  in
+      Arg.(value & opt (some string) None & info ["uobj"; "uobject"] ~docs ~docv:"UOBJECT" ~doc)
+  in
+
 	let baseinv =
-    let doc = "verify only uberspark base invariants." in
+    let doc = "Verify only uberspark base invariants." in
     Arg.(value & flag & info ["b"; "baseinv"] ~doc ~docs)
 
   in
-  Term.(const cmd_verify_opts_handler $ baseinv)
+
+
+  Term.(const cmd_verify_opts_handler $ uobject $ baseinv)
 
 
 
@@ -50,6 +61,24 @@ let handler_verify
     (* no manifest file found in current directory, display cli help and exit *)
     (`Help (`Pager, None))
   else
+
+  let l_cmd_verify_opts_list = ref [] in
+  (* process options *)
+  let dummy=0 in begin
+  l_cmd_verify_opts_list := !l_cmd_verify_opts_list @ [ ("baseinv", string_of_bool(p_cmd_verify_opts.baseinv)) ];
+  match p_cmd_verify_opts.uobject with
+  | None ->
+    l_cmd_verify_opts_list := !l_cmd_verify_opts_list @ [ ("uobject", "") ];    
+  | Some l_uobject_name -> 
+    l_cmd_verify_opts_list := !l_cmd_verify_opts_list @ [ ("uobject", l_uobject_name) ];
+  ;
+  
+  (*List.iter ( fun (x, y) -> 
+  	Uberspark.Logger.log ~lvl:Uberspark.Logger.Debug "x: %s, y: %s" x y;
+  )!l_cmd_verify_opts_list;
+  *)
+  end;       
+
 
   (* create and initialize operation context by processing manifest *)
   let l_rval = (Common.initialize_operation_context_with_staging ~p_in_order:false 
